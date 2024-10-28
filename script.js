54dae397acd03aeaf099e96ab27038211ed73fdf
NhatVu <113049836+nhatvux@users.noreply.github.com>
2024-10-28 13:31:55 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function addToCart(productName, price) {
-    alert(`${productName} đã được thêm vào giỏ hàng với giá ${price} VNĐ.`);
+function changeQuantity(productId, change) {
+    const quantityInput = document.getElementById(`quantity-${productId}`);
+    let currentQuantity = parseInt(quantityInput.value);
+    currentQuantity += change;
+
+    if (currentQuantity < 1) {
+        currentQuantity = 1; // Đảm bảo số lượng không âm
+    }
+
+    quantityInput.value = currentQuantity;
+}
+
+function addToCart(productName, price, quantity) {
+    alert(`${productName} đã được thêm vào giỏ hàng với giá ${price} VNĐ và số lượng ${quantity}.`);
 }
